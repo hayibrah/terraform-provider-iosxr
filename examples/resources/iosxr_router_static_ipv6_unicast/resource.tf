a1bf593cac521afe48c060e433129bf73e48fb6b
rocrowe <rocrowe@cisco.com>
2026-09-08 14:30:27 -0400

    chore(gen): reformat examples/docs via terraform fmt
    Restore terraform fmt alignment across all ~230 example and data-source
    files lost during the chore(gen) regeneration commit (3ccdca8d). Also
    removes a stale hostname.md Length annotation left from a temporary
    string-length test that was never backed by a committed definition change.
    
    Co-authored-by: Robert Crowe <rocrowe@cisco.com>

--- examples/resources/iosxr_router_static_ipv6_unicast/resource.tf
+++ examples/resources/iosxr_router_static_ipv6_unicast/resource.tf
@@ -1,97 +1,97 @@
 resource "iosxr_router_static_ipv6_unicast" "example" {
   prefix_address = "1::"
-  prefix_length = 64
+  prefix_length  = 64
   nexthop_interfaces = [
     {
-      interface_name = "GigabitEthernet0/0/0/1"
-      description = "interface-description"
-      tag = 100
+      interface_name  = "GigabitEthernet0/0/0/1"
+      description     = "interface-description"
+      tag             = 100
       distance_metric = 122
-      permanent = true
-      metric = 10
+      permanent       = true
+      metric          = 10
     }
   ]
   nexthop_interface_addresses = [
     {
-      interface_name = "GigabitEthernet0/0/0/2"
-      address = "2::2"
+      interface_name                   = "GigabitEthernet0/0/0/2"
+      address                          = "2::2"
       bfd_fast_detect_minimum_interval = 100
-      bfd_fast_detect_multiplier = 3
-      description = "interface-description"
-      tag = 103
-      distance_metric = 144
-      metric = 10
+      bfd_fast_detect_multiplier       = 3
+      description                      = "interface-description"
+      tag                              = 103
+      distance_metric                  = 144
+      metric                           = 10
     }
   ]
   nexthop_addresses = [
     {
-      address = "3::3"
+      address                          = "3::3"
       bfd_fast_detect_minimum_interval = 100
-      bfd_fast_detect_multiplier = 3
-      description = "ip-description"
-      tag = 104
-      distance_metric = 155
-      track = "TRACK1"
-      metric = 10
+      bfd_fast_detect_multiplier       = 3
+      description                      = "ip-description"
+      tag                              = 104
+      distance_metric                  = 155
+      track                            = "TRACK1"
+      metric                           = 10
     }
   ]
   sr_policies = [
     {
-      sr_policy_name = "sr_te_policy_1"
-      description = "interface-description"
-      tag = 103
+      sr_policy_name  = "sr_te_policy_1"
+      description     = "interface-description"
+      tag             = 103
       distance_metric = 144
-      track = "TRACK1"
-      metric = 10
+      track           = "TRACK1"
+      metric          = 10
     }
   ]
   vrfs = [
     {
       vrf_name = "VRF1"
-        nexthop_interfaces = [
-          {
-            interface_name = "GigabitEthernet0/0/0/3"
-            description = "interface-description"
-            tag = 100
-            distance_metric = 122
-            permanent = true
-            metric = 10
-          }
-        ]
-        nexthop_interface_addresses = [
-          {
-            interface_name = "GigabitEthernet0/0/0/4"
-            address = "2::2"
-            bfd_fast_detect_minimum_interval = 100
-            bfd_fast_detect_multiplier = 3
-            description = "interface-description"
-            tag = 103
-            distance_metric = 144
-            metric = 10
-          }
-        ]
-        nexthop_addresses = [
-          {
-            address = "3::3"
-            bfd_fast_detect_minimum_interval = 100
-            bfd_fast_detect_multiplier = 3
-            description = "ip-description"
-            tag = 104
-            distance_metric = 155
-            track = "TRACK1"
-            metric = 10
-          }
-        ]
-        sr_policies = [
-          {
-            sr_policy_name = "sr_te_policy_1"
-            description = "interface-description"
-            tag = 103
-            distance_metric = 144
-            track = "TRACK1"
-            metric = 10
-          }
-        ]
+      nexthop_interfaces = [
+        {
+          interface_name  = "GigabitEthernet0/0/0/3"
+          description     = "interface-description"
+          tag             = 100
+          distance_metric = 122
+          permanent       = true
+          metric          = 10
+        }
+      ]
+      nexthop_interface_addresses = [
+        {
+          interface_name                   = "GigabitEthernet0/0/0/4"
+          address                          = "2::2"
+          bfd_fast_detect_minimum_interval = 100
+          bfd_fast_detect_multiplier       = 3
+          description                      = "interface-description"
+          tag                              = 103
+          distance_metric                  = 144
+          metric                           = 10
+        }
+      ]
+      nexthop_addresses = [
+        {
+          address                          = "3::3"
+          bfd_fast_detect_minimum_interval = 100
+          bfd_fast_detect_multiplier       = 3
+          description                      = "ip-description"
+          tag                              = 104
+          distance_metric                  = 155
+          track                            = "TRACK1"
+          metric                           = 10
+        }
+      ]
+      sr_policies = [
+        {
+          sr_policy_name  = "sr_te_policy_1"
+          description     = "interface-description"
+          tag             = 103
+          distance_metric = 144
+          track           = "TRACK1"
+          metric          = 10
+        }
+      ]
     }
   ]
 }
--- examples/resources/iosxr_service_timestamps/resource.tf
+++ examples/resources/iosxr_service_timestamps/resource.tf
@@ -1,12 +1,12 @@
 resource "iosxr_service_timestamps" "example" {
-  debug_datetime_localtime = true
+  debug_datetime_localtime     = true
   debug_datetime_show_timezone = true
-  debug_datetime_year = true
-  debug_uptime = true
-  log_datetime_localtime = true
-  log_datetime_show_timezone = true
-  log_datetime_year = true
-  log_uptime = true
-  debug_datetime_usec = true
-  log_datetime_usec = true
+  debug_datetime_year          = true
+  debug_uptime                 = true
+  log_datetime_localtime       = true
+  log_datetime_show_timezone   = true
+  log_datetime_year            = true
+  log_uptime                   = true
+  debug_datetime_usec          = true
+  log_datetime_usec            = true
 }
